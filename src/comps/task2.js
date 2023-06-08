73736559cdce1d1bf5dca2a40709f95145150fdd
reutcohe <116076322+reutcohe@users.noreply.github.com>
2023-06-08 11:34:54 +0300

    aa

--- src/comps/task2.js
+++ src/comps/task2.js
@@ -1,17 +1,35 @@
-// import { render } from "@testing-library/react";
-// import React,{Component} from "react";
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
 
-// export default class Task2 extends Component{
-//      state = {pics:["cake1.jpg","cake2.jpg"]};
-// count=0;
+export default class Task2 extends Component {
+    state = { pics: ["cake1.jpg", "cake2.jpg","cake3.jpg","cake4.jpg","cake5.jpg"], count: 0 };
 
-//     render(){
-//         return(
-//  <div>
+    addCount = () => {
+        let countCheng=this.state.count+1
+        if(countCheng>4){
+         countCheng=0
+        }
+          this.setState({count:countCheng});
+      }
+      lessCount = () => {
+  
+       let countCheng=this.state.count-1
+       if(countCheng<0){
+        countCheng=4
+       }
+         this.setState({count:countCheng});
+      }
 
 
-// <img src={"/images/"+this.state.pics[count]} width="200"/>
-// </div> 
-//         )
-//     }
-// }
+    render() {
+        return (
+            <div>
+
+               <img src={"/images/" + this.state.pics[this.state.count]}  width="200"  height="200"/>
+                <hr />
+                <button onClick={this.lessCount}>back</button>
+                <button onClick={this.addCount}>next</button>
+            </div>
+        )
+    }
+}
