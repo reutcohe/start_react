fcf615a338f4054fc26619e0854974514922a562
reutcohe <116076322+reutcohe@users.noreply.github.com>
2023-06-08 09:21:10 +0300

    start

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Counter from './comps/counter';
 import InputVal from './comps/inputVal';
 import Message from'./comps/message'
 import Task1 from'./comps/task1'
-
+import Task2 from'./comps/task2'
 function App() {
   return (
 
 <div className='countainer'>
   <h1 className='text-denger'>React</h1>
+  <Task2/>
   <Task1/>
   <Message txt="first"/>
   <InputVal />
---
+++ src/comps/task2.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import React,{Component} from "react";
+
+export default class Task2 extends Component{
+     state = {pics:["cake1.jpg","cake2.jpg"]};
+count=0;
+
+    render(){
+        return(
+<div>
+
+
+<img src={"/images/"+this.state.pics[count]} width="200"/>
+</div>
+        )
+    }
+}
