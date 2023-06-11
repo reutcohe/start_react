1e6068866ef5030f915846bdfb5c59b9f573bde8
reutcohe <116076322+reutcohe@users.noreply.github.com>
2023-06-11 10:17:17 +0300

    r

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
 import AppHw from './comps/appHw';
-import ShopList from './comps_loop/shopList';
+// import ShopList from './comps_loop/shopList';
 
 function App() {
  return(
-  // <React.Fragment>
-  <>
-    <h1 className='text-danger'>React</h1>
-    <hr/>
+  //  <React.Fragment>
+  // <>
+    // <h1 className='text-danger'>React</h1>
+    // <hr/>
     <AppHw/>
-    <hr/>
-  <ShopList/>
-    </>
-  // </React.Fragment>
+    // <hr/>
+  /* <ShopList/> */
+  //   </>
+  //  </React.Fragment>
  )
 }
 
